942a533f3e04cd9e8aa9203afe5c69fe90333163
AlexandrKoverza <alexandrkoverza@gmail.com>
2021-02-24 17:07:11 +0200

    test

---
+++ тест/js/main.js
@@ -0,0 +1,7 @@
+let lang = document.querySelector('.select__block')
+let select__language  = document.querySelector('.select__language')
+
+lang.addEventListener('click', () => {
+    select__language.style.opacity = '1'
+})
+
