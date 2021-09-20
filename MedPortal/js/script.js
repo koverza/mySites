1e1d30f2834a900bb8e7abaa88f4c82707fc2cfb
AlexandrKoverza <alexandrkoverza@gmail.com>
2021-09-20 16:13:05 +0300

    сайт

--- MedPortal/js/script.js
+++ MedPortal/js/script.js
@@ -1,3 +1,31 @@
+// SEARCH
+let buttons = document.querySelectorAll('*[data-popup-btn]')
+
+for (let index = 0; index < buttons.length; index++) {
+    buttons[index].addEventListener('click', function() {
+        let name = buttons[index].getAttribute('data-popup-btn')
+        let popup = document.querySelector("[data-popup-window='"+name+"']")
+        popup.style.opacity = '1'
+        popup.style.visibility = 'visible'
+
+        let popupClose = popup.querySelector('.popup__close')
+        popupClose.addEventListener('click', function() {
+            popup.style.opacity = '0'
+            popup.style.visibility = 'hidden'
+        })
+    })
+}
+
+window.onclick = function(e) {
+    if (e.target.hasAttribute('data-popup-window')) {
+        let popups = document.querySelectorAll("*[data-popup-window]")
+        for (let index = 0; index < popups.length; index++) {
+            popups[index].style.opacity = '0'
+            popups[index].style.visibility = 'hidden'
+        }
+    }
+};
+
 // HEADER
 
 let burger = document.querySelector('.burger')
@@ -238,32 +266,6 @@ reviewsButton.addEventListener('click', () => {
 
 
 
-// let buttons = document.querySelectorAll('*[data-popup-btn]')
-
-for (let index = 0; index < buttons.length; index++) {
-    buttons[index].addEventListener('click', function() {
-        let name = buttons[index].getAttribute('data-popup-btn')
-        let popup = document.querySelector("[data-popup-window='"+name+"']")
-        popup.style.opacity = '1'
-        popup.style.visibility = 'visible'
-
-        let popupClose = popup.querySelector('.popup__close')
-        popupClose.addEventListener('click', function() {
-            popup.style.opacity = '0'
-            popup.style.visibility = 'hidden'
-        })
-    })
-}
-
-window.onclick = function(e) {
-    if (e.target.hasAttribute('data-popup-window')) {
-        let popups = document.querySelectorAll("*[data-popup-window]")
-        for (let index = 0; index < popups.length; index++) {
-            popups[index].style.opacity = '0'
-            popups[index].style.visibility = 'hidden'
-        }
-    }
-};
 // const accordionItemHeaders = document.querySelectorAll('.accordion-item-header')
 
 accordionItemHeaders.forEach(accordionItemHeader => {
