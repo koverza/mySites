075dd578bd3bd63edbca3fa30aaf83feb3c56a7e
AlexandrKoverza <alexandrkoverza@gmail.com>
2022-03-11 17:46:45 +0200

    delete old sites

--- тест/js/main.js
+++
@@ -1,7 +0,0 @@
-let lang = document.querySelector('.select__block')
-let select__language  = document.querySelector('.select__language')
-
-lang.addEventListener('click', () => {
-    select__language.style.opacity = '1'
-})
-
