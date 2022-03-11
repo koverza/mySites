bcd1387c4560f33e4f1793e32ca43ac851d75759
AlexandrKoverza <alexandrkoverza@gmail.com>
2022-03-11 17:48:08 +0200

    add gulp-2020 and gulp-2022

---
+++ gulp-2022/src/js/anchors.js
@@ -0,0 +1,12 @@
+let anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+    anchor.addEventListener('click', (event) => {
+        event.preventDefault()
+        const blockID = anchor.getAttribute('href')
+        document.querySelector('' + blockID).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        })
+    })
+}
---
+++ gulp-2022/src/js/app.js
@@ -0,0 +1,23 @@
+import * as flsFunctions from "./modules/functions.js";
+flsFunctions.isWebp();
+
+/*
+import Swiper, { Navigation, Pagination } from 'swiper';
+const swiper = new Swiper();
+*/
+
+/* SWIPER 
+
+const name = new Swiper('.', {
+    loop: true,
+    navigation: {
+        nextEl: '.',
+        prevEl: '.',
+    },
+    pagination: {
+        el: '.,
+    },
+})
+
+*/
+
