28ec854e090f7abb2fe0236440210093e2d17260
AlexandrKoverza <alexandrkoverza@gmail.com>
2022-09-05 23:21:57 +0300

    update gulp 05.09.2022

---
+++ gulp-2022/src/js/toTop.js
@@ -0,0 +1,12 @@
+// Button to top
+window.onscroll = () => {
+    // Button to top
+    const toTop = document.querySelector('.toTop')
+    const Y = window.scrollY
+
+    if (Y > window.innerHeight) {
+        toTop.classList.add('toTop-fixed')
+    } else {
+        toTop.classList.remove('toTop-fixed')
+    }
+}
